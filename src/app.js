5eea5f33a9b93c08a037f33196d7f63e66cfade7
Alexander Molnar <mathic@gmail.com>
2018-06-27 16:18:48 -0400

    Adjusted code to pass FCC test suite

--- src/app.js
+++ src/app.js
@@ -5,10 +5,8 @@ window.onscroll = function(e) {
   if (window.scrollY > 0) {
     let header = document.getElementById('header')
     header.style.background = 'white';
-    header.style.height = '60px';
   } else if (window.scrollY === 0) {
     document.getElementById('header').style.background = 'lightBlue';
-    header.style.height = '80px';
   }
 }
 
